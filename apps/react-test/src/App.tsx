76223179ba33def8961a1d1cafad5660c083b16f
Salino3 <flaviobcn012@gmail.com>
2026-05-05 14:51:05 +0200

    using shared interface

--- apps/react-test/src/App.tsx
+++ apps/react-test/src/App.tsx
@@ -1,11 +1,13 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from './assets/vite.svg'
-import heroImg from './assets/hero.png'
-import './App.css'
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "./assets/vite.svg";
+import heroImg from "./assets/hero.png";
+import { type User } from "@project/shared-types";
+import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [userData, setUserData] = useState<User | null>(null);
+  const [count, setCount] = useState(0);
 
   return (
     <>
@@ -116,7 +118,7 @@ function App() {
       <div className="ticks"></div>
       <section id="spacer"></section>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
